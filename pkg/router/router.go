35b1b6611a3dd5f3929aaf7b828771b92963c678
Lucas Schilin <schilin.lucas@gmail.com>
2025-05-15 05:46:45 -0300

    fix: register root routes in router setup
    The tests were passing, but in practice it wasn't working. The main wasn't registering the routes

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -2,10 +2,13 @@ package router
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/lucasschilin/schily-users-api/internal/root"
 )
 
 func New() *mux.Router {
 	r := mux.NewRouter()
+
+	root.RegisterRoutes(r)
 
 	return r
 }
