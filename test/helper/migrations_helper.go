fa23ba6574b84c7ee31ae58b4721de7576bf475d
Lucas Schilin <schilin.lucas@gmail.com>
2025-05-26 07:06:08 -0300

    test: setup dbs test containers  to use in endpoints test

---
+++ test/helper/migrations_helper.go
@@ -0,0 +1,27 @@
+package helper
+
+import (
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrationsUp(dbURL string, migrationsPath string) error {
+	m, err := migrate.New(
+		fmt.Sprintf("file://%s", migrationsPath),
+		dbURL,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
