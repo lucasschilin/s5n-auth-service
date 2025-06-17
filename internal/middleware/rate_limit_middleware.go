5c0b5c7c988121e903e39e1ed390e29a49652a07
Lucas Schilin <schilin.lucas@gmail.com>
2025-06-17 06:33:19 -0300

    feat: implement rate limiting middleware
    Configured to allow 2 requests per second.

--- internal/middleware/rate_limit_middleware.go
+++ internal/middleware/rate_limit_middleware.go
@@ -3,23 +3,25 @@ package middleware
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
 	"time"
 
 	"github.com/lucasschilin/s5n-auth-service/internal/cache"
+	"github.com/lucasschilin/s5n-auth-service/internal/dto"
 )
 
 const (
-	limit                 = 3
-	windowDurationSeconds = 10
+	limit                 = 2
+	windowDurationSeconds = 1
 )
 
 func RateLimit(cache cache.Cache) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			prefix := "ratelimit"
+			prefix := "rate_limit"
 			remoteAddr := r.RemoteAddr
 			ip, _, _ := net.SplitHostPort(remoteAddr)
 
@@ -27,21 +29,49 @@ func RateLimit(cache cache.Cache) func(http.Handler) http.Handler {
 			if r.UserAgent() != "" {
 				userAgent = r.UserAgent()
 			}
-
-			ui := userIdentifierHash(ip + userAgent)
-
-			key := fmt.Sprintf("%s:%s", prefix, ui)
+			key := fmt.Sprintf(
+				"%s:%s", prefix, userIdentifierHash(ip+userAgent),
+			)
 
 			now := time.Now().Unix()
 			startWindow := now - windowDurationSeconds
 
-			reqs, err := cache.Get(key)
-			if err != nil {
-				fmt.Println(err)
+			var reqs []int64
+
+			val, _ := cache.Get(key)
+			if val == "" {
+				val = "[]"
 			}
 
-			fmt.Println(reqs, startWindow)
-			// TODO: Continuar implementação (converter valor para json, verificar length, adicionar unix() time, ...)
+			json.Unmarshal([]byte(val), &reqs)
+			reqs = append(reqs, now)
+
+			firstReqInWindow := 0
+
+			for i, r := range reqs {
+				if r >= startWindow {
+					break
+				}
+
+				firstReqInWindow = i + 1
+
+			}
+
+			reqs = reqs[firstReqInWindow:]
+
+			reqsBytes, _ := json.Marshal(reqs)
+
+			cache.Set(
+				key, string(reqsBytes), (windowDurationSeconds+5)*time.Second,
+			)
+
+			if len(reqs) > limit {
+				w.WriteHeader(http.StatusTooManyRequests)
+				json.NewEncoder(w).Encode(dto.DefaultDetailResponse{
+					Detail: "Too many requests.",
+				})
+				return
+			}
 
 			next.ServeHTTP(w, r)
 		})
